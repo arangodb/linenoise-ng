30d97e1d40798a01b2235ac9254590f16308c271
Teddy Reed <teddy@prosauce.org>
2016-10-10 21:06:01 -0700

    [c894b9e] Fix insecure history file creation

--- src/linenoise.cpp
+++ src/linenoise.cpp
@@ -105,6 +105,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/ioctl.h>
 #include <cctype>
@@ -3265,11 +3266,18 @@ char* linenoiseHistoryLine(int index) {
 /* Save the history in the specified file. On success 0 is returned
  * otherwise -1 is returned. */
 int linenoiseHistorySave(const char* filename) {
+#ifndef _WIN32
+  mode_t old_umask = umask(S_IXUSR|S_IRWXG|S_IRWXO);
+#endif
   FILE* fp = fopen(filename, "wt");
   if (fp == NULL) {
     return -1;
   }
 
+#ifndef _WIN32
+  umask(old_umask);
+  chmod(filename,S_IRUSR|S_IWUSR);
+#endif
   for (int j = 0; j < historyLen; ++j) {
     if (history[j][0] != '\0') {
       fprintf(fp, "%s\n", history[j]);
